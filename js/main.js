8756db54b98570021f1fedb4a7862d92a41bf583
Tanya-tech-tech <lee-tanya2insp@yandex.ru>
2020-06-13 16:08:42 +0300

    ДОбавила правильный путь к main.js

--- js/main.js
+++ js/main.js
@@ -5,7 +5,8 @@ var CHECKOUT = ['12:00', '13:00', '14:00'];
 var TYPE = ['palace', 'flat', 'house', 'bungalo'];
 var FACILITIES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg',
-'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg']
+'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+
 var map = document.querySelector('.map');
 map.classList.remove('map--faded');
 
